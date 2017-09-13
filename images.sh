baf472802426c208034515c55cd86ca823dcc2d0
David Salamon <dls@lithp.org>
2017-09-13 09:17:19 -0700

    checkpoint

---
+++ images.sh
@@ -0,0 +1,4 @@
+dot -Tpng src/figures/agoraOverview.dot -o images/agoraOverview.png
+dot -Tpng src/figures/sttc.dot -o images/sttc.png
+dot -Tpng src/figures/sttRttc.dot -o images/sttRttc.png
+dot -Tpng src/figures/xorSpace.dot -o images/xorSpace.png
