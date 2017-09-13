af858fb2ff130d5db18ef46a211aec9c87cb2a5a
Brian J. Fox <bfox@opuslogica.com>
2017-09-13 07:58:07 -0700

    WIP: Getting build scripts for mac/linux command line, etc

---
+++ README.md
@@ -0,0 +1,9 @@
+# Our Whitepaper
+
+This is the whitepaper describing the Agora secure network protocol.
+
+## To Build
+
+   ./build.sh
+
+
---
+++ build.sh
@@ -0,0 +1,16 @@
+# Note: this script runs pdflatex, bibtex, then pdflatex again because of how generating bibliographies works:
+
+# Run LaTeX on your document. This creates a .aux file that BibTeX uses to look for references according to the “key”.
+# Run BibTeX on your document. This creates a .bbl file that LaTeX uses to create your bibliography.
+# Run LaTeX again to update the document with the reference list and all the right citations.
+
+export PATH=$PATH:/Library/TeX/texbin
+document=whitepaper
+rm ${document}.pdf
+pdflatex ${document}
+bibtex ${document}
+pdflatex ${document}
+pdflatex ${document}
+pdflatex ${document}
+rm -f *.aux  *.blg  *.dvi  *.log *.lot *.lof *.toc *.gz *.bbl
+# evince ${document}.pdf
